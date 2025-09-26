736980563a53c2226d8be69f1452fc76e8a01fdd
Wei Ting TAN <weitingtan14@gmail.com>
2025-09-26 14:15:29 -0400

    fixcode

--- string_reversal.py
+++ string_reversal.py
@@ -19,7 +19,7 @@ def reverse_string(mystring):
         return []
 
     # BUG: the 0 in the range() method should be -1, given how range() works!
-    for i in range(len(mystring) - 1, -1, -1):
+    for i in range(len(mystring) - 1, 0, -1):
         reversed_string += mystring[i]
 
     return reversed_string
